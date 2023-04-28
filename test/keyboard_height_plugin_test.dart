4f4a551217165f61a65f46a47168d265c1de6636
Noah Schairer <nschairer@gmail.com>
2023-04-28 15:23:54 -0400

    remove some tests

--- test/keyboard_height_plugin_test.dart
+++ test/keyboard_height_plugin_test.dart
@@ -1,29 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:keyboard_height_plugin/keyboard_height_plugin.dart';
-import 'package:keyboard_height_plugin/keyboard_height_plugin_platform_interface.dart';
-import 'package:keyboard_height_plugin/keyboard_height_plugin_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockKeyboardHeightPluginPlatform
-    with MockPlatformInterfaceMixin
-    implements KeyboardHeightPluginPlatform {
-
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-}
-
 void main() {
-  final KeyboardHeightPluginPlatform initialPlatform = KeyboardHeightPluginPlatform.instance;
-
-  test('$MethodChannelKeyboardHeightPlugin is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelKeyboardHeightPlugin>());
-  });
-
-  test('getPlatformVersion', () async {
-    KeyboardHeightPlugin keyboardHeightPlugin = KeyboardHeightPlugin();
-    MockKeyboardHeightPluginPlatform fakePlatform = MockKeyboardHeightPluginPlatform();
-    KeyboardHeightPluginPlatform.instance = fakePlatform;
-
-    expect(await keyboardHeightPlugin.getPlatformVersion(), '42');
-  });
 }
